0c03c3f653010b489b8ac84b2be8d7cad4870e4d
Irla Svendsen <irla@MacBook-Pro-3.local>
2019-11-07 13:33:25 -0700

    Altered from promiss to async await

--- place-list/routes/places.js
+++ place-list/routes/places.js
@@ -4,59 +4,55 @@ const Place = require('../models/places.model');
 
 router.get('/place', (req, res) => {res.send('place')});
 
-router.route('/add').post( (req, res) => {
-    const newPlace = new Place(req.body)
-    newPlace.save().then(place => {
-        res.json('Place added with success!'+ place);
-        }, error => {
-            res.status(400).send('unable to create a new place');
-        });
-});
-
-router.route('/delete').delete((req, res) => {
-    console.log('inicializing deletion...');
-    Place.deleteOne(req.body, (error) => {
-        if (error){
-            console.log('Error'+error);
-            res.status(400).send('Error trying to delete an item');
-        } 
-    })
-    res.json('Item deleted successfull');
-});
-
-router.get('/home/name/:placeName', async (req, res) => {
+router.route('/add').post( async (req, res, next) => {
     try {
-       const placeName = req.params.placeName;
-       console.log('looking for the place: '+req.params.placeName);
+        const newPlace = new Place(req.body)
+        const place = await newPlace.save();
+        res.json('Place added with success!'+ place);
+           
+    } catch(error) {
+        next(error);
+    }
     
-      await Place.find({'name': placeName});
-      res.send(places);
+});
+
+router.route('/delete').delete( async (req, res, next) => {
+    try {
+        console.log('inicializing deletion...');
+        await Place.deleteOne(req.body);
+        res.json('Item deleted successfull');
     } catch(error) {
         next(error);
     }
 });
 
-router.route('/alter').patch((req, res)=>{
-    const placeName = req.body.name;
-    console.log('looking for the place: '+placeName);
+router.get('/home/name/:placeName', async (req, res, next) => {
+    try {
+    const placeName = req.params.placeName;
+    console.log('looking for the place: '+req.params.placeName);
     
-    Place.findOne({'name': placeName}, (err, place) => {
-        if(err) {
-            console.log('error'+err);
-            res.status(400).send(err);
-        }
-        console.log(place);
+    const place = await Place.find({'name': placeName});
+      res.send(place);
+    } catch(error) {
+        next(error);
+    }
+});
+
+router.route('/alter').patch( async (req, res, next) => {
+    try {
+        const placeName = req.body.name;
+        console.log('looking for the place: '+placeName);
+        
+        const place = await Place.findOne({'name': placeName});
         if(place) {
             place.visited = req.body.visited;
             console.log(place);
-            place.save( (err) => {
-                if (err) return res.status(400).send(err); // saved!
-             });
-             res.send('Upadate Successfull'+place);
+            await place.save();
+            res.send('Upadate Successfull'+place);
         }
-        //res.status(400).send('place not found');
-        
-      });
+    } catch(error) {
+        next(error);
+    }
 })
 
 module.exports = router;
